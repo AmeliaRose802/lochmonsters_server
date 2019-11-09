5ebd2da3d9b6759fdfd1d6771568ef16341e1efd
Amelia <amelia.payne@mymail.champlain.edu>
2019-11-09 02:12:41 +0000

    Now correctly recves connect message and records new client

--- src/AcceptConnection.kt
+++ src/AcceptConnection.kt
@@ -1,13 +1,19 @@
-import java.*
 import java.io.*
 import java.net.*
 const val portNum = 5555
 
 //Client and server exchange messages of set types each of which has a set format and length
-val messageLengths = mapOf('c' to 33);
+val messageLengths = mapOf('c' to 39);
 
-data class Color(val r : Int, val b: Int, val g: Int);
+data class Vector2(val x : Float, val y : Float);
 
+data class Color(val r : Short, val b: Short, val g: Short);
+
+data class Client(val name: String, val color: Color, val pos : Vector2, val dir : Vector2);
+
+val clients = mutableMapOf<Int, Client>()
+
+var nextID : Int = 0;
 
 fun main(args: Array<String>) {
     println("Server is listening...")
@@ -26,25 +32,8 @@ fun main(args: Array<String>) {
 
         //Yes, I will be figuring out something more elegent later
         if(type === 'c'){
-
-            val r = dis.readShort();
-            val g = dis.readShort();
-            val b = dis.readShort();
-            println("The color is $r $b $g");
-
-            val name : ByteArray = ByteArray(40);
-
-            println(dis.read(name, 13, length));
-
-            /*
-            var sholdRun = true;
-            while(sholdRun){
-                val data = dis.read();
-                println(data.toChar());
-                sholdRun = (data != -1);
-            }*/
-            //val c = Color(dis.read(),dis.read(),dis.read());
-            //println("c: $c");
+            AddClient(dis);
+            SendConnectReply(s);
         }
 
 
@@ -54,4 +43,35 @@ fun main(args: Array<String>) {
     } catch (e: Exception) {
         println(e)
     }
+}
+
+//Add a new player to the game
+fun AddClient(dis : DataInputStream) : Int{
+    val r = dis.readShort();
+    val g = dis.readShort();
+    val b = dis.readShort();
+    println("The color is $r $g $b");
+
+    val name : ByteArray = ByteArray(32);
+
+    dis.read(name)
+
+    val playerName = String(name).trim();
+    println(playerName);
+
+    val currentID = nextID;
+    clients.put(nextID, Client(playerName, Color(r, g, b), Vector2(0f,0f), Vector2(0f,0f)))
+    println(clients[nextID]);
+
+    nextID++;
+
+    return currentID;
+}
+
+fun SendConnectReply(clientSocket : Socket){
+    val dos = DataOutputStream(clientSocket.getOutputStream())
+    dos.writeChar('c'.toInt())
+    dos.writeFloat(0f)
+    dos.writeFloat(0f)
+    dos.flush()
 }
