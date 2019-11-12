879dd036db13dea60f3c9e29e9a3cc44f2e472fb
Amelia <amelia.payne@mymail.champlain.edu>
2019-11-12 23:44:27 +0000

    Now sends data about all snakes in game to newly connected clients

--- src/AcceptConnection.kt
+++ src/AcceptConnection.kt
@@ -1,12 +1,8 @@
 import java.io.*
-import java.net.*
-import java.net.DatagramPacket
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.net.InetSocketAddress
-import sun.nio.ch.IOUtil.configureBlocking
 import java.net.InetAddress
-import jdk.nashorn.internal.objects.ArrayBufferView.buffer
 import java.nio.channels.*
 import kotlin.random.Random
 
@@ -23,7 +19,8 @@ class Server() {
     //Data classes
     data class Vector2(var x: Float, var y: Float);
     data class Color(val r: Short, val b: Short, val g: Short);
-    data class Client(val name: String, val color: Color, var pos: Vector2, var dir: Vector2);
+    data class Snake(val name: String, val color: Color, var length : Short, var pos: Vector2, var dir: Vector2);
+    data class Client(val snake : Snake);
 
     //Currently Connected clients (eg monsters in the game)
     val clients = mutableMapOf<Int, Client>()
@@ -52,6 +49,9 @@ class Server() {
         udpSocketChannel.register(selector, SelectionKey.OP_READ)
         val key: SelectionKey? = null
         buffer.order(ByteOrder.LITTLE_ENDIAN)
+        clients.put(0, Client(Snake("Test", Color(11,17,19), 3, Vector2(0.5f, 1.5f), Vector2(0.0f, 0.0f))))
+        clients.put(1, Client(Snake("bob", Color(8,256,3), 5, Vector2(7.0f, 4.0f), Vector2(1.0f, 0.5f))))
+        nextID++
     }
 
     fun listen() {
@@ -71,10 +71,8 @@ class Server() {
                         val client = serverSocketChannel.accept()
                         client.configureBlocking(false);
                         client.register(selector, SelectionKey.OP_READ);
-                        println("Regestered new connection")
                     }
                     if (key.isReadable) {
-                        println("Data to read");
 
                         if (key.channel() is SocketChannel) {
                             getTCPPacket(key.channel() as SocketChannel, key)
@@ -94,34 +92,28 @@ class Server() {
 
 
     fun getUDPPackets(socket: DatagramChannel) {
-        println("UDP");
         socket.receive(buffer)
         buffer.position(0);
         val type = buffer.getChar();
-        println(String(buffer.array(), 0, buffer.array().size))
-        println("type= $type")
         when (type) {
             'p' -> {
-                println("Position update")
                 handlePosUpdate(buffer);
             }
         }
     }
 
     fun getTCPPacket(socket: SocketChannel, key: SelectionKey) {
-        println("TCP");
         socket.read(buffer);
         buffer.position(0);
         val type = buffer.getChar();
-        println(String(buffer.array(), 0, buffer.array().size))
-        println("type= $type")
+
         when (type) {
             'c' -> {
                 println("Connection request")
                 val id = AddClient(buffer)
 
-                SendConnectReply(socket, id); //Probley should be checking if write will block but oh well
-
+                sendConnectReply(socket, id); //Probley should be checking if write will block but oh well
+                sendAllOtherPlayerData(socket, id); //TODO: Ideally this would only send closest clients
             }
         }
     }
@@ -129,20 +121,17 @@ class Server() {
 
     fun handlePosUpdate(data: ByteBuffer) {
         val id = data.getInt();
-        println(id);
 
         if (clients[id] == null) {
             println("Unknown Client");
             return;
         }
 
-        clients[id]!!.pos.x = data.float;
-        clients[id]!!.pos?.y = data.float;
+        clients[id]!!.snake.pos.x = data.float;
+        clients[id]!!.snake.pos?.y = data.float;
 
-        clients[id]!!.dir?.x = data.float;
-        clients[id]!!.dir?.y = data.float;
-
-        println(clients[id]);
+        clients[id]!!.snake.dir?.x = data.float;
+        clients[id]!!.snake.dir?.y = data.float;
     }
 
     //Add a new player to the game
@@ -150,17 +139,15 @@ class Server() {
         val r = buffer.short
         val g = buffer.short
         val b = buffer.short
-        println("The color is $r $g $b");
 
         val name: ByteArray = ByteArray(32);
 
         buffer.get(name);
 
         val playerName = String(name).trim();
-        println(playerName);
 
         val currentID = nextID;
-        clients.put(currentID, Client(playerName, Color(r, g, b), Vector2(0f, 0f), Vector2(0f, 0f)))
+        clients.put(currentID, Client(Snake(playerName, Color(r, g, b),3, Vector2(0f, 0f), Vector2(0f, 0f))))
         println(clients[currentID]);
 
         nextID++;
@@ -168,8 +155,8 @@ class Server() {
         return currentID;
     }
 
-    fun SendConnectReply(clientSocket: SelectableChannel, id: Int) {
-        val reply: ByteArray = ByteArray(21)
+    fun sendConnectReply(clientSocket: SelectableChannel, id: Int) {
+        val reply: ByteArray = ByteArray(14)
 
         val x: Int = Random.nextInt(0, 100); //Eventually this will pick a spot not near other snakes
         val y: Int = Random.nextInt(0, 100);
@@ -183,7 +170,49 @@ class Server() {
 
         val c = clientSocket as SocketChannel;
         c.write(ByteBuffer.wrap(reply))
+    }
 
-        println("Data sent");
+    fun sendAllOtherPlayerData(clientSocket: SelectableChannel, myID : Int){
+        println("Attempting to send other player data");
+        val s = clientSocket as SocketChannel
+        val c = ByteBuffer.allocate(4 + (60 * (clients.size -1)));
+        println("Capacity of c "+ c.array().size);
+        c.order(ByteOrder.LITTLE_ENDIAN)
+
+//        clients.forEach { t, u -> {
+//            println(t)
+//            println(u)
+//        } }
+
+        c.putChar('o')
+
+        c.putShort((clients.size - 1).toShort());
+
+        for ((id, client) in clients) {
+            //Only send data for other clients
+            if(id != myID) {
+                println("$id = $client")
+
+                c.putInt(id) //Add ID
+                c.put(client.snake.name.padEnd(32).toByteArray()) //Name
+                println(""+client.snake.name.padEnd(32).toByteArray() + "!");
+                c.putShort(client.snake.length) //Length of snake
+                c.putShort(client.snake.color.r) //R of color
+                c.putShort(client.snake.color.g) //R of color
+                c.putShort(client.snake.color.b) //R of color
+                c.putFloat(client.snake.pos.x)
+                c.putFloat(client.snake.pos.y)
+                c.putFloat(client.snake.dir.x)
+                c.putFloat(client.snake.dir.y)
+
+            }
+        }
+
+
+        s.write(ByteBuffer.wrap(c.array())); //I have no god damm idea why this works!
+
+
+        //s.write(c);
+
     }
 }
